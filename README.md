1171de161378a7c51230df621f90a9abc55c7f5f
1BObC <bob.cocksedge@gmail.com>
2021-09-13 23:05:29 +0100

    Complete function basics and functions inside functions Start create your own function

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # fundsPt3
 Mainly js function basics
 "Create folder and files"
+"Complete function basics and functions inside functions Start create your own function"
--- jscript.js
+++ jscript.js
@@ -1,1 +1,56 @@
 // mainly function basics
+//mdn webdocs function playing with scope exercise
+// let x = 1;
+
+//     function a() {
+//       let y = 2;
+//       // output(y);
+//       //output(x);
+//       output(z);
+
+//     }
+
+//     function b() {
+//       let z = 3;
+//       // output(z);
+//       //output(x);
+//       output(y);
+//     }
+
+//     function output(value) {
+//       const para = document.createElement('p');
+//       document.body.appendChild(para);
+//       para.textContent = 'Value: ' + value;
+//     }
+
+//functions inside functions
+  // function myBigFunction(){
+  //   let myValue1 = 1;
+  //   let myValue2 = 2;
+  //   let myValue3 = 3;
+
+  //   subFunction1(myValue1);
+  //   subFunction2(myValue2);
+  //   subFunction3(myValue3);
+  // }
+
+  // function subFunction1(value){
+  //   console.log(value);
+  //   document.getElementById('bob1').innerHTML = `The value of bob1 is: ${value}`;
+  // }
+  // function subFunction2(value){
+  //   console.log(value);
+  //   document.getElementById('bob2').innerHTML = `The value of bob2 is: ${value}`;
+  // }
+  // function subFunction3(value){
+  //   console.log(value);
+  //   document.getElementById('bob3').innerHTML = `The value of bob3 is: ${value}`;
+  // }
+
+  //   myBigFunction();
+
+    // Let's build a function - why not?
+    function displayMessage(){
+      
+
+    }
